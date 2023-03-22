236b9412389f356bfdad2cbcbece7d11fb892dde
qxg <2569729045@qq.com>
2023-03-22 22:32:03 +0800

    完善社区接口，修改过滤器bug

--- HomePainter/src/main/java/com/homepainter/filter/GlobalExceptionHandler.java
+++ HomePainter/src/main/java/com/homepainter/filter/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.homepainter.filter;
 
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -47,11 +48,21 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public Map<String, Object> SocketHandler(IOException e){
+    public Map<String, Object> AllHandler(Exception e){
+        e.printStackTrace();
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", 50);
+        map.put("msg", "其他错误！");
+        return map;
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    @ResponseBody
+    public Map<String, Object> Handler(HttpMediaTypeNotSupportedException e){
         e.printStackTrace();
         Map<String, Object> map = new HashMap<>();
         map.put("code", 7);
-        map.put("msg", "其他错误！");
+        map.put("msg", "媒体错误，我用的是requestbody，不要传form——data，会报错，我只能收json");
         return map;
     }
 
