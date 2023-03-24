d8a0499073abec8ff7fd09440e3a1d634036b65f
qxg <2569729045@qq.com>
2023-03-24 11:58:08 +0800

    翻译接口

--- HomePainter/src/main/java/com/homepainter/service/TranslateService.java
+++ HomePainter/src/main/java/com/homepainter/service/TranslateService.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Service
 public class TranslateService {
 
+    public static long translationCount = 0;
     public List <JSONObject> translateJson(List <JSONObject> jsonObjects) throws Exception {
         for (JSONObject jsonObject : jsonObjects){
             jsonObject.put("super-category", Translate.translateEnToZh((String) jsonObject.get("super-category")));
@@ -18,6 +19,8 @@ public class TranslateService {
             jsonObject.put("style", Translate.translateEnToZh((String) jsonObject.get("style")));
             jsonObject.put("material", Translate.translateEnToZh((String) jsonObject.get("material")));
             jsonObject.put("theme", Translate.translateEnToZh((String) jsonObject.get("theme")));
+            translationCount++;
+            System.out.println("图片翻译进行数目：" + translationCount);
         }
 ;
         return jsonObjects;
